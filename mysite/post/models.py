e5140a192248f0aaffb4c28b2a9055ecc4f72409
haru <haruki6180@gmail.com>
2021-07-04 22:18:15 +0900

    #タグ機能完成

--- mysite/post/models.py
+++ mysite/post/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=20)
     text = models.CharField(max_length=200)
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
--- mysite/post/views.py
+++ mysite/post/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
-from .models import Post
+from django.shortcuts import redirect
+from .models import Post, Tag
 
 
 class ListClass(ListView):
@@ -14,3 +15,16 @@ class FormClass(CreateView):
     fields = ('title', 'text')
     success_url = reverse_lazy('list')
 
+    def form_valid(self, form):
+        post = Post(
+            title=form.cleaned_data["title"], text=form.cleaned_data["text"])
+        post.save()
+        words = form.cleaned_data["text"].split()
+        for word in words:
+            if word[0] == "#":
+                if Tag.objects.filter(name=word[1:]).exists():
+                    tag = Tag.objects.get(name=word[1:])
+                else:
+                    tag = Tag.objects.create(name=word[1:])
+                post.tag.add(tag)
+        return redirect('list')
