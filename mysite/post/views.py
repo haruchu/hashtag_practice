874acba3fc7e691cf043c81b7384c7e622fb07ae
haru <haruki6180@gmail.com>
2021-07-04 16:29:49 +0900

    投稿機能できた

--- mysite/post/views.py
+++ mysite/post/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Post
 
-# Create your views here.
+
+class ListClass(ListView):
+    template_name = 'list.html'
+    model = Post
+
+
+class FormClass(CreateView):
+    template_name = 'form.html'
+    model = Post
+    fields = ('title', 'text')
+    success_url = reverse_lazy('list')
+
